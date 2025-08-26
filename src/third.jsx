891c97a5e4cfc7a3977efdb4e5ab2beec919f483
MatveySinitckiy <sinitckiymatvey@gmail.com>
2025-08-26 23:57:31 +0500

    fixing

--- src/third.jsx
+++ src/third.jsx
@@ -10,7 +10,7 @@ function thirdstep(){
             <p className=" text-2xl font-light text-black p-[1vmin]" >Создай персонализированный план занятий, который идеально впишется в твой ритм жизни.</p>
 
         </div>
-        <div className="w-[50%] h-[100%] items-center  justify-center flex shadow-2xl shadow-stone-950/80"><img src={'public/joga33.jpg'} className="border-purple-200"  alt="Скачай приложение" ></img></div>
+        <div className="w-[50%] h-[100%] items-center  justify-center flex shadow-2xl shadow-stone-950/80"><img src='./joga33.jpg' className="border-purple-200"  alt="Скачай приложение" ></img></div>
     </div>
     
     
